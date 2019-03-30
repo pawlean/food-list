f7ca3aa87b99a90f0df32cb524644b64d49d9482
Matt Burman <matt@mattburman.com>
2019-03-30 13:09:45 +0000

    Add foods to Mock API

--- api/mock/list.js
+++ api/mock/list.js
@@ -1,11 +1,14 @@
 const zeroToN = n => Math.round(Math.random() * (n || 100));
+const foods = require('./foods');
 
 const generateMacros = () => {
   const carbs = zeroToN(20);
   const protein = zeroToN(40);
   const fat = 100 - carbs - protein;
 
-  return { fat, protein, carbs };
+  var name = foods[Math.floor(Math.random()*foods.length)];
+
+  return { fat, protein, carbs, name };
 };
 
 module.exports = () => {
