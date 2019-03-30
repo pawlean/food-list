8f3085b99d16529e7026c7ad88571222611aca27
Matt Burman <matt@mattburman.com>
2019-03-30 18:46:07 +0000

    Add macro service and split into two services

---
+++ api/macro/index.js
@@ -0,0 +1,22 @@
+const urllib = require('url');
+const tescoScrape = require('./scraper/tesco');
+
+module.exports = (req, res) => {
+  const query = urllib.parse(req.url, true).query;
+  const url = query.url;
+
+  if (!url || !url.contains('https://www.tesco.com/groceries/en-GB/products/')) {
+    res.statusCode = 400;
+    return res.end();
+  }
+
+  tescoScrape(url)
+    .then(data => {
+      if (data && data.name) return res.end(data);
+      res.statusCode = 404;
+      res.end("Tesco data not found.");
+    }).catch(err => {
+      res.statusCode = 500;
+      res.end("Failed to get data from Tesco.");
+    });
+};
