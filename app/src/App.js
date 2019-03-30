1575503834be1a191fb90b8a80854b4eb8d094b9
Pauline P. Narvas <paulinenarvas@gmail.com>
2019-03-30 11:24:24 +0000

    Update styling

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './custom.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <h1>😋 Food List</h1>
+
+        <table>
+        <tr>
+        <th class="food-title">Food</th>
+        <th class="carbs">Carbs</th>
+        <th class="fat">Fat</th>
+        <th class="protein">Protein</th>
+        </tr>
+        <tr>
+        <td>Cheese</td>
+        <td>70%</td>
+        <td>20%</td>
+        <td>10%</td>
+        </tr>
+        <tr>
+        <td>Bread</td>
+        <td>70%</td>
+        <td>20%</td>
+        <td>10%</td>
+        </tr>
+        <tr>
+        <td>Milk</td>
+        <td>70%</td>
+        <td>20%</td>
+        <td>10%</td>
+        </tr>
+        </table>
         </header>
+        <footer>
+        <p>Created with 💜 by <a href="https://twitter.com/_mattburman">Matt</a> & <a href="https://twitter.com/paulienuh">Pauline</a></p>
+        </footer>
       </div>
     );
   }
