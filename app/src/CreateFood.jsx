4883daf9d909e9e63b191eff750614a8d251c3bd
Pauline P. Narvas <paulinenarvas@gmail.com>
2019-03-30 20:17:21 +0000

    Add URL Change Handler

--- app/src/CreateFood.jsx
+++ app/src/CreateFood.jsx
@@ -1,25 +1,56 @@
 import React from 'react';
+import axios from 'axios';
 
+export default class CreateFood extends React.Component {
+  state = {
+    url: '',
+    name: '',
+    fat: '',
+    protein: '',
+    carbs: ''
+  }
 
-function CreateFood() {
+handleURLChange(event) {
+  const url = event.target.value;
+
+  if(!url.startsWith('https://www.tesco.com/groceries/en-GB/products/')) return;
+
+  axios.get(`/api/macro?url=${url}`)
+    .then(res => {
+      const { name, fat, protein, carbs } = res.data;
+      this.setState({ url, name,
+        fat: fat.toFixed(2),
+        protein: protein.toFixed(2),
+        carbs: carbs.toFixed(2) });
+    })
+    .catch(error => {
+      alert(error.data);
+    })
+}
+
+render() {
   return(
     <div class="FoodForm">
 
     <form>
 
       <label>URL of product
-        <input type="text" name="name" />
+        <input type="text" name="name" onChange={this.handleURLChange.bind(this)}/>
+      </label>
+
+      <label>Name of product
+        <input type="text" name="name" value={this.state.name}/>
       </label>
 
       <label>% fat</label>
-        <input type="text" name="name" id="text1" />
+        <input type="text" name="name" id="text1" value={this.state.fat} />
 
       <label>% protein</label>
-        <input type="text" name="name" id="text1"  />
+        <input type="text" name="name" id="text1" value={this.state.protein} />
 
 
       <label>% carbs</label>
-        <input type="text" name="name" id="text1" />
+        <input type="text" name="name" id="text1" value={this.state.carbs} />
 
         <input type="submit" value="Submit" />
 
@@ -29,4 +60,4 @@ function CreateFood() {
   )
 }
 
-export default CreateFood;
+}
